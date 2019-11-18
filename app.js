355209d80ab4f6756d63fb679075cd49bd58c7eb
Deividas <deivis124@gmail.com>
2019-11-18 04:34:07 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,56 @@
+var bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  express = require("express"),
+  app = express();
+
+// APP CONFIG
+mongoose.connect("mongodb://localhost:27017/restful_blog_app", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  //   image: { type: String, default: "placeholderimage.jps" },
+  body: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+/* Blog.create({
+  title: "Test Bllog",
+  image:
+    "https://images.unsplash.com/photo-1574012437553-8fda4179ae82?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+  body: "HELLO THIS IS A BLOG POST"
+});
+ */
+
+// RESTFUL ROUTES
+
+app.get("/", function(req, res) {
+  res.redirect("/blogs");
+});
+
+app.get("/blogs", function(req, res) {
+  Blog.find({}, function(err, blogs) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { blogs: blogs });
+    }
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Listening on port " + this.address().port);
+});
